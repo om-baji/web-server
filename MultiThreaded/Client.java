774b3aad5515ac28985ee66b7ecea642f49bf66d
om-baji <ombaji124@gmail.com>
2025-05-26 21:19:33 +0530

    Multithreaded:

---
+++ MultiThreaded/Client.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.rmi.server.ExportException;
+
+public class Client {
+
+    public Runnable getRunnable() {
+        return new Runnable() {
+            @Override
+            public void run(){
+                int port = 8000;
+
+                try {
+                    InetAddress address = InetAddress.getByName("localhost"); 
+                    Socket socket =  new Socket(address,port);
+                    
+                    try (
+                        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(),true);
+                    BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    ) {
+                    
+                        toSocket.println("Hello from client!");
+                        String line = fromSocket.readLine();
+
+                        System.out.println("Received from server " + line);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+    public static void main(String[] args) {
+        Client client = new Client();
+        
+        for(int i = 0; i < 100; i++) {
+            try {
+                Thread thread = new Thread(client.getRunnable());
+                thread.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ MultiThreaded/Server.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
+
+public class Server {
+
+    public Consumer<Socket> getConsumer() {
+        return (clientSocket -> {
+            try {
+                PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream());
+                toClient.println("Hello from server!");
+                toClient.close();
+                clientSocket.close();   
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public static void main(String[] args) {
+        int port = 8000;
+        Server server = new Server();
+        try {
+            ServerSocket socket = new ServerSocket(port);
+            socket.setSoTimeout(10000);
+            System.out.println("Server listening on port " + port);
+            while (true) {
+                Socket acceptedSocket = socket.accept();
+                Thread thread = new Thread(() -> server.getConsumer().accept(acceptedSocket));
+                thread.start();
+            }   
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
